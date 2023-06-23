946ebba90133c3d6b1c2781d202f268eb241947a
KoulteghdevOps <koultegh@outlook.com>
2023-06-23 10:57:13 +0100

    manifest pushed

--- terraform.tf
+++ terraform.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = module.vpc.cidr_blocks #[aws_vpc.main.cidr_block]
+    cidr_blocks = module.vpc.cidr #[aws_vpc.main.cidr_block]
   }
 
   ingress {
@@ -46,7 +46,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = module.vpc.cidr_blocks #[aws_vpc.main.cidr_block]
+    cidr_blocks = module.vpc.cidr #[aws_vpc.main.cidr_block]
   }
 
   egress {
